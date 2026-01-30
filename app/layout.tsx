5b111bbb25d62f07ff00f136768e3ca750e57ebf
w4nnn <irwanfebriansyah112@gmail.com>
2026-01-30 07:08:42 +0700

    feat: Set up base Next.js layout with Geist fonts, metadata, and Toaster component.

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,8 +14,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "English Learning AI",
-  description: "Belajar Bahasa Inggris dengan AI",
+  title: "English Learning",
+  description: "Belajar Bahasa Inggris dengan",
 };
 
 export default function RootLayout({
